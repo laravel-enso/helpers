5464e4ffaf70e45fd7320e85ce420dbeec422153
aocneanu <aocneanu@gmail.com>
2017-03-19 13:37:46 +0000

    Apply fixes from StyleCI

--- src/Classes/AbstractObject.php
+++ src/Classes/AbstractObject.php
@@ -4,19 +4,18 @@ namespace LaravelEnso\Helpers\Classes;
 
 abstract class AbstractObject
 {
+    public function __toString()
+    {
+        return $this->toJson();
+    }
 
-	public function __toString()
-	{
-		return $this->toJson();
-	}
+    public function toJson()
+    {
+        return json_encode($this);
+    }
 
-	public function toJson()
-	{
-		return json_encode($this);
-	}
-
-	public function toArray()
-	{
-		return (array) $this;
-	}
+    public function toArray()
+    {
+        return (array) $this;
+    }
 }
