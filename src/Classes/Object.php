bc710184fb89011b9b8be88496465fcafe386327
mishu <mishu@xtelecom.ro>
2017-07-09 22:13:01 +0300

    added auto constructor to Object

--- src/Classes/Object.php
+++ src/Classes/Object.php
@@ -4,4 +4,10 @@ namespace LaravelEnso\Helpers\Classes;
 
 class Object extends AbstractObject
 {
+	public function __construct(array $properties = [])
+    {
+        foreach ($properties as $property => $value) {
+            $this->$property = $value;
+        }
+    }
 }
