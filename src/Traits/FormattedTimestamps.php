721a8d010c48faef4db42fb802ab489a1085acf3
mishu <mishu@xtelecom.ro>
2017-08-01 20:16:59 +0300

    v1.2

--- src/Traits/FormattedTimestamps.php
+++ src/Traits/FormattedTimestamps.php
@@ -8,11 +8,15 @@ trait FormattedTimestamps
 {
     public function getUpdatedAtAttribute($value)
     {
-        return $value ? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps')) : null;
+        return $value
+        	? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
+        	: null;
     }
 
     public function getCreatedAtAttribute($value)
     {
-        return $value ? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps')) : null;
+        return $value
+        	? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
+        	: null;
     }
 }
