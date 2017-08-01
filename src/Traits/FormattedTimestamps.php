b2b4cbe9bda76b73ba2843fb5e7a2c704451d25e
Adrian Ocneanu <aocneanu@gmail.com>
2017-08-01 17:17:17 +0000

    Apply fixes from StyleCI

--- src/Traits/FormattedTimestamps.php
+++ src/Traits/FormattedTimestamps.php
@@ -9,14 +9,14 @@ trait FormattedTimestamps
     public function getUpdatedAtAttribute($value)
     {
         return $value
-        	? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
-        	: null;
+            ? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
+            : null;
     }
 
     public function getCreatedAtAttribute($value)
     {
         return $value
-        	? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
-        	: null;
+            ? Carbon::parse($value)->format(config('laravel-enso.formattedTimestamps'))
+            : null;
     }
 }
