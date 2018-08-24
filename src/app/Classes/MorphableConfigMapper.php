11a5b5d5d130c2f77a9235f1d18b5d4d0b716e43
mishu <mishu@xtelecom.ro>
2018-08-24 11:13:36 +0300

    adds morphable config reader

---
+++ src/app/Classes/MorphableConfigMapper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace LaravelEnso\Helpers\app\Classes;
+
+use LaravelEnso\Helpers\app\Exceptions\MorphableConfigException;
+
+abstract class MorphableConfigMapper
+{
+    protected $configPrefix;
+    protected $morphableKey;
+    protected $type;
+    protected $morphable;
+
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+        $this->morphable = $this->morphable();
+    }
+
+    public function model()
+    {
+        return is_array($this->morphable)
+            ? $this->morphable['model']
+            : $this->morphable;
+    }
+
+    private function morphable()
+    {
+        $morphable = config($this->configPrefix.'.'.$this->morphableKey.'.'.$this->type);
+
+        if (is_null($morphable)) {
+            throw new MorphableConfigException(__(
+                'Entity ":entity" does not exist in ":config" config file',
+                ['entity' => $this->type, 'config' => $this->configFile()]
+            ));
+        }
+
+        if (!is_string($morphable) || !class_exists($morphable)) {
+            throw new MorphableConfigException(__(
+                'Given class for ":entity" in ":config" config file does not exist',
+                ['entity' => $this->type, 'config' => $this->configFile()]
+            ));
+        }
+
+        return $morphable;
+    }
+
+    private function configFile()
+    {
+        return str_replace('.', '/', $this->configPrefix).'.php';
+    }
+}
