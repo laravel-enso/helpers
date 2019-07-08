aa3366a7dfcee5712ea1f51f8cf90bb970863e64
mishu <mishu@xtelecom.ro>
2019-07-08 19:25:48 +0300

    adds UpdateOnTouch trait

---
+++ src/app/Traits/UpdateOnTouch.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LaravelEnso\Helpers\app\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+trait UpdateOnTouch
+{
+    public function touchOwners()
+    {
+        foreach ($this->touches as $relation) {
+            $this->$relation()->touch();
+
+            if ($this->$relation instanceof Model) {
+                $this->$relation->fireModelEvent('saved', false);
+                $this->$relation->fireModelEvent('updated', false);
+
+                $this->$relation->touchOwners();
+            } elseif ($this->$relation instanceof Collection) {
+                $this->$relation->each(function (Model $relation) {
+                    $relation->touchOwners();
+                });
+            }
+        }
+    }
+}
