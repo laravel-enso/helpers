acdc61e1ae73f7aaa9c41e72d893ace3080c5796
mishu <mishu@xtelecom.ro>
2019-07-15 17:50:11 +0300

    adds inCents trait

---
+++ src/app/Traits/InCents.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaravelEnso\FiscalDocuments\app\Traits;
+
+trait InCents
+{
+    //protected $centAttributes = [ ];
+
+    protected $inCents = true;
+
+    public function inCents($mode = true)
+    {
+        if ($this->inCents === $mode) {
+            return false;
+        }
+
+        $this->inCents = $mode;
+
+        collect($this->centAttributes)
+            ->each(function ($field) {
+                $this->attributes[$field] = $this->inCents
+                    ? $this->attributes[$field] * 100
+                    : $this->attributes[$field] / 100;
+            });
+
+        return true;
+    }
+}
