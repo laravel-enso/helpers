e5c6b50ee5fa0165a55be5999508514a57d0c16a
mishu <mishu@xtelecom.ro>
2019-10-23 14:50:27 +0300

    updates strategy for dateattributes

--- src/app/Traits/DateAttributes.php
+++ src/app/Traits/DateAttributes.php
@@ -3,17 +3,28 @@
 namespace LaravelEnso\Helpers\app\Traits;
 
 use Carbon\Carbon;
+use InvalidArgumentException;
 
 trait DateAttributes
 {
     public function fillDateAttribute($attribute, $value, $format = null)
     {
-        $format = $format
-            ?? config('enso.config.dateFormat')
-            ?? $this->getDateFormat();
+        if (! $value || $value instanceof Carbon) {
+            $this->attributes[$attribute] = $value;
 
-        $this->attributes[$attribute] = $value && ! $value instanceof Carbon
-            ? Carbon::createFromFormat($format, $value)
-            : $value;
+            return;
+        }
+
+        try {
+            $format = $format
+                ?? config('enso.config.dateFormat')
+                ?? $this->getDateFormat();
+
+            $value = Carbon::createFromFormat($format, $value);
+        } catch (InvalidArgumentException $e) {
+            $value = Carbon::parse($value);
+        } finally {
+            $this->attributes[$attribute] = $value;
+        }
     }
 }
