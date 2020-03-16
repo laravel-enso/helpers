ecdcec4d8049fecbe42c5b35315a0f486f8a3803
mishu <mishu@xtelecom.ro>
2020-03-16 18:04:46 +0200

    adds When helper trait

---
+++ src/app/Traits/When.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaravelEnso\Helpers\App\Traits;
+
+trait When
+{
+    public function when($value, $callback, $default = null)
+    {
+        if ($value) {
+            return $callback($this, $value) ?: $this;
+        } elseif ($default) {
+            return $default($this, $value) ?: $this;
+        }
+
+        return $this;
+    }
+}
