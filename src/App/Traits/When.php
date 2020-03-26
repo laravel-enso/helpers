f85c8c264c2796cc19fb4a0f932094e9d7529111
mishu <mishu@xtelecom.ro>
2020-03-26 17:32:14 +0200

    refactors When

--- src/App/Traits/When.php
+++ src/App/Traits/When.php
@@ -2,14 +2,16 @@
 
 namespace LaravelEnso\Helpers\App\Traits;
 
+use Closure;
+
 trait When
 {
-    public function when($value, $callback, $default = null)
+    public function when($value, Closure $callback, ?Closure $else = null)
     {
         if ($value) {
             return $callback($this, $value) ?: $this;
-        } elseif ($default) {
-            return $default($this, $value) ?: $this;
+        } elseif ($else) {
+            return $else($this, $value) ?: $this;
         }
 
         return $this;
