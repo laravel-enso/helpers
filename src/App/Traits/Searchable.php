7cd3cb12d572616109071c62a848d17bd0fca689
mishu <mishu@xtelecom.ro>
2020-05-10 16:39:28 +0300

    adds searchable trait for scout

---
+++ src/App/Traits/Searchable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaravelEnso\Helpers\App\Traits;
+
+use Laravel\Scout\Searchable as ScoutSearchable;
+
+trait Searchable
+{
+    use ScoutSearchable;
+
+    public function save(array $options = [])
+    {
+        if ($this->shouldPerformSearchSyncing()) {
+            return parent::save($options);
+        }
+
+        static::disableSearchSyncing();
+        $result = parent::save($options);
+        static::enableSearchSyncing();
+
+        return $result;
+    }
+
+    private function shouldPerformSearchSyncing()
+    {
+        return ! empty(array_intersect_key($this->getDirty(), $this->toSearchableArray()));
+    }
+}
