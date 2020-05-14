d2672dda411e72a30bb4cfb167291d426b26332a
mishu <mishu@xtelecom.ro>
2020-05-14 12:06:01 +0300

    improves maps request keys while keeping the old method for backward compatibility

--- src/App/Traits/MapsRequestKeys.php
+++ src/App/Traits/MapsRequestKeys.php
@@ -13,4 +13,26 @@ trait MapsRequestKeys
             ->mapWithKeys(fn ($value, $key) => [Str::snake($key) => $value])
             ->toArray();
     }
+
+    public function withValidator($validator)
+    {
+        $this->toSnakeCaseKeys($validator);
+    }
+
+    public function validated()
+    {
+        return $this->snakeCaseKeys($this->validator->validated());
+    }
+
+    private function toSnakeCaseKeys($validator): void
+    {
+        $validator->after(fn () => $this->replace($this->snakeCaseKeys($this->all())));
+    }
+
+    private function snakeCaseKeys(array $request): array
+    {
+        return (new Collection($request))
+            ->mapWithKeys(fn ($value, $key) => [Str::snake($key) => $value])
+            ->toArray();
+    }
 }
