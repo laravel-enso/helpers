145acee1fdc72b8197abf32cc5f3704d0e6fe723
Mihai <mihai.ocneanu@gmail.com>
2020-05-15 10:26:44 +0300

    added moved the vat rates enum from the financials package

---
+++ src/App/Enums/VatRates.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaravelEnso\Helpers\App\Enums;
+
+use LaravelEnso\Enums\App\Services\Enum;
+
+class VatRates extends Enum
+{
+    public const None = 0;
+    public const Five = 5;
+    public const Nine = 9;
+    public const Nineteen = 19;
+}
