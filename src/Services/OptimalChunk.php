b1c3948af416ab97f92ddb6122151f60319a12e1
mishu <mishu@xtelecom.ro>
2020-12-09 19:22:09 +0200

    adds OptimalChunk

---
+++ src/Services/OptimalChunk.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaravelEnso\Helpers\Services;
+
+use Illuminate\Support\Collection;
+
+class OptimalChunk
+{
+    public const Thresholds = [
+        ['limit' => 1000, 'chunk' => 100],
+        ['limit' => 10 * 1000, 'chunk' => 250],
+        ['limit' => 50 * 1000, 'chunk' => 2 * 500],
+        ['limit' => 250 * 1000, 'chunk' => 4 * 500],
+        ['limit' => 1000 * 1000, 'chunk' => 10 * 500],
+    ];
+
+    public const MaxChunk = 10000;
+
+    public static function get(int $count, int $min): int
+    {
+        $match = (new Collection(self::Thresholds))
+            ->first(fn ($threshold) => $count <= $threshold['limit']);
+
+        $limit = $match ? $match['chunk'] : self::MaxChunk;
+
+        return min($limit, $min);
+    }
+}
