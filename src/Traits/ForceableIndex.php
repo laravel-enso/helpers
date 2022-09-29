e276a36d828fdbb3ef9d5c1c1bd9d539d9cae98f
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2022-09-29 10:54:31 +0300

    adds use index ability in forceable index trait

--- src/Traits/ForceableIndex.php
+++ src/Traits/ForceableIndex.php
@@ -9,10 +9,18 @@ trait ForceableIndex
 {
     public function scopeForceIndex(Builder $query, string $index): Builder
     {
-        $query->getQuery()
-            ->from(
-                DB::raw("{$query->getQuery()->from} FORCE INDEX ({$index})")
-            );
+        $raw = DB::raw("{$query->getQuery()->from} FORCE INDEX ({$index})");
+
+        $query->getQuery()->from($raw);
+
+        return $query;
+    }
+
+    public function scopeUseIndex(Builder $query, string $index): Builder
+    {
+        $raw = DB::raw("{$query->getQuery()->from} USE INDEX ({$index})");
+
+        $query->getQuery()->from($raw);
 
         return $query;
     }
