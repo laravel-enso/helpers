c4d7a8c53c114a1cc57f7d61a23fd58dd96fc597
mishu <mishu@xtelecom.ro>
2025-04-08 14:27:44 +0300

    makes default scale 4

--- src/Services/Decimals.php
+++ src/Services/Decimals.php
@@ -4,7 +4,7 @@ namespace LaravelEnso\Helpers\Services;
 
 class Decimals
 {
-    private static int $scale = 2;
+    private static int $scale = 4;
 
     public static function scale(?int $precision = null): int
     {
