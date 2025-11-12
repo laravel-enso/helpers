b85ff4b463f168dff2167955be71d8182af59100
mishu <mishu@xtelecom.ro>
2025-11-12 14:34:27 +0200

    fixes disksize

--- src/Services/DiskSize.php
+++ src/Services/DiskSize.php
@@ -9,7 +9,7 @@ class DiskSize
         $threshholds = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
         $base = 1024;
         $threshold = min((int) log($bytes, $base), count($threshholds) - 1);
-        $size = Decimals::div($bytes, Decimals::pow($base, $threshold));
+        $size = Decimals::div($bytes, Decimals::pow($base, $threshold), 1);
 
         return "{$size} {$threshholds[$threshold]}";
     }
