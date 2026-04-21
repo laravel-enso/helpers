d7530abb468fffffacfeab44a5b99b0be67f6427
mishu <mishu@xtelecom.ro>
2026-04-21 18:16:29 +0300

    Add testing folder helper trait

---
+++ src/Traits/EnsuresTestingFolder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelEnso\Helpers\Traits;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+trait EnsuresTestingFolder
+{
+    protected function ensureTestingFolder(): void
+    {
+        $folder = Config::get('enso.files.testingFolder');
+
+        if (! $folder) {
+            return;
+        }
+
+        $path = Storage::path($folder);
+
+        if (! File::isDirectory($path)) {
+            File::makeDirectory($path, 0755, true, true);
+        }
+    }
+}
